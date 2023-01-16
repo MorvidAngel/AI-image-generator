d502454fa081cdcd311d2ff7f2610a7e35d796bd
MorvidAngel <5d7n2s9m6b@live.com>
2023-01-16 13:30:15 -0500

    ignore files

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -6,9 +6,32 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
-  res.status(200).json({
-    success: true,
-  });
+  try {
+    const response = await openai.createImage({
+      prompt: 'Polar bear on ice skates',
+      n: 1,
+      size: '512x512',
+    });
+
+    const imageUrl = respose.data.data[0].url;
+
+    res.status(200).json({
+      success: true,
+      data: imageUrl,
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+
+    res.status(400).json({
+      success: false,
+      error: 'The image could not be generated',
+    });
+  }
 };
 
 module.exports = { generateImage };
